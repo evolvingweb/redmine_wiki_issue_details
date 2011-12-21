92823c20bfa7dfc8d277174a55bf01550a0e4745
Thomas Getgood <thomas@evolvingweb.ca>
2011-12-21 12:27:12 -0500

    Properly added 'id' as a shortcut for 'issue_details'. The macros also take some arguments now to display moreinfo.

--- rails/init.rb
+++ rails/init.rb
@@ -20,11 +20,21 @@ Dispatcher.to_prepare :redmine_wiki_issue_details do
   Issue.send(:include, RedmineWikiIssueDetails::IssueCompatibilityPatch) unless Issue.respond_to? :visible
 end
 
+
+Redmine::Plugin.register :redmine_wiki_issue_details do
+  name 'Redmine Wiki Issue Details plugin'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'This plugin adds a wiki macro to make it easier to list the details of issues on a wiki page.'
+  version '0.1.0'
+  requires_redmine :version_or_higher => '0.8.0'
+
   Redmine::WikiFormatting::Macros.register do
     desc "Display an issue and it's details.  Examples:\n\n" +
       "  !{{issue_details(100)}}\n\n" +
       "  Digitized 24 hour firmware - Bug #391 Robust disintermediate customer loyalty - 25.23 hours"
-    macro :issue_details do |obj, args|
+     block = lambda do |obj, args|
       issue_id = args[0]
       issue = Issue.visible.find_by_id(issue_id)
 
@@ -41,44 +51,39 @@ end
       end
 
       project_link = link_to(h(issue.project), :controller => 'projects', :action => 'show', :id => issue.project)
-        
-      returning '' do |response|
-        response << '<span style="text-decoration: line-through;">' if issue.closed?
-        response << project_link
-        response << ' - '
-        response << link_to_issue(issue) + ' '
-        response << estimates + ' '
-        response << "(#{h(issue.status)})"
-        response << '</span>' if issue.closed?
-      end
-    end
 
-    macro :id do |obj, args|
-      issue_id = args[0]
-      issue = Issue.visible.find_by_id(issue_id)
+      # Collect extra information in case we need to display it.
+      priority = IssuePriority.find_by_id(issue.priority_id).name
 
-      return '' unless issue
-
-      if Redmine::AccessControl.permission(:view_estimates) && !User.current.allowed_to?(:view_estimates, issue.project)
-        # Check if the view_estimates permission is defined and the user
-        # is allowed to view the estimate
-        estimates = ''
-      elsif issue.estimated_hours && issue.estimated_hours > 0
-        estimates = "- #{l_hours(issue.estimated_hours)}"
+      if IssueCategory.find_by_id(issue.category_id) then
+        category = IssueCategory.find_by_id(issue.category_id).name
       else
-        estimates = "- <strong>#{l(:redmine_wiki_issue_details_text_needs_estimate)}</strong>"
+        category = "<strong>No Category</strong>"
       end
 
-      project_link = link_to(h(issue.project), :controller => 'projects', :action => 'show', :id => issue.project)
+      if User.find_by_id(issue.assigned_to_id) then 
+        assignee = User.find_by_id(issue.assigned_to_id).name
+      else 
+        assingee = "<strong>Unassigned</strong>"
+      end
+      
+      # Remove whitespace from args for better parsing.
+      args.collect{|x| x.strip!}
         
       returning '' do |response|
         response << '<span style="text-decoration: line-through;">' if issue.closed?
         response << project_link
+        response << ' <' + category + '>' if args[1..-1].include? 'version'
         response << ' - '
+        response << "(" + priority + ")" + ' ' if args[1..-1].include? 'priority'
         response << link_to_issue(issue) + ' '
         response << estimates + ' '
         response << "(#{h(issue.status)})"
+        response << ": " + assignee if args[1..-1].include? 'assignee'
         response << '</span>' if issue.closed?
       end
     end
+    macro :issue_details, &block
+    macro :id, &block
   end
+end
