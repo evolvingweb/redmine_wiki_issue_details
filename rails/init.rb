117b56e487e6a7a1c72e495c79478973678c2239
Thomas Getgood <thomas@evolvingweb.ca>
2011-12-21 10:14:11 -0500

    Hack for id. will fix.

--- rails/init.rb
+++ rails/init.rb
@@ -20,17 +20,6 @@ Dispatcher.to_prepare :redmine_wiki_issue_details do
   Issue.send(:include, RedmineWikiIssueDetails::IssueCompatibilityPatch) unless Issue.respond_to? :visible
 end
 
-
-Redmine::Plugin.register :redmine_wiki_issue_details do
-  name 'Redmine Wiki Issue Details plugin'
-  author 'Eric Davis'
-  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
-  author_url 'http://www.littlestreamsoftware.com'
-  description 'This plugin adds a wiki macro to make it easier to list the details of issues on a wiki page.'
-  version '0.1.0'
-  requires_redmine :version_or_higher => '0.8.0'
-
-
   Redmine::WikiFormatting::Macros.register do
     desc "Display an issue and it's details.  Examples:\n\n" +
       "  !{{issue_details(100)}}\n\n" +
@@ -63,5 +52,33 @@ Redmine::Plugin.register :redmine_wiki_issue_details do
         response << '</span>' if issue.closed?
       end
     end
+
+    macro :id do |obj, args|
+      issue_id = args[0]
+      issue = Issue.visible.find_by_id(issue_id)
+
+      return '' unless issue
+
+      if Redmine::AccessControl.permission(:view_estimates) && !User.current.allowed_to?(:view_estimates, issue.project)
+        # Check if the view_estimates permission is defined and the user
+        # is allowed to view the estimate
+        estimates = ''
+      elsif issue.estimated_hours && issue.estimated_hours > 0
+        estimates = "- #{l_hours(issue.estimated_hours)}"
+      else
+        estimates = "- <strong>#{l(:redmine_wiki_issue_details_text_needs_estimate)}</strong>"
+      end
+
+      project_link = link_to(h(issue.project), :controller => 'projects', :action => 'show', :id => issue.project)
+        
+      returning '' do |response|
+        response << '<span style="text-decoration: line-through;">' if issue.closed?
+        response << project_link
+        response << ' - '
+        response << link_to_issue(issue) + ' '
+        response << estimates + ' '
+        response << "(#{h(issue.status)})"
+        response << '</span>' if issue.closed?
+      end
+    end
   end
-end
